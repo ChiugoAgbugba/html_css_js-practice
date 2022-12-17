331ebef33b44a0b637820c82117bb35082e615d6
Chiugo Agbugba <chiugo.agbugba@gmail.com>
2022-12-17 13:39:09 +0100

    initial commit

---
+++ assets/main.js
@@ -0,0 +1,75 @@
+const users = [
+	{
+		firstName: 'John',
+		lastName: 'Doe',
+		yearOfBirth: 1984,
+		gender: 'male',
+	},
+	{
+		firstName: 'Mark',
+		lastName: 'Bonner',
+		yearOfBirth: 1964,
+		gender: 'male',
+	},
+	{
+		firstName: 'Jane',
+		lastName: 'Doe',
+		yearOfBirth: 1974,
+		gender: 'female',
+	},
+	{
+		firstName: 'Beth',
+		lastName: 'Butter',
+		yearOfBirth: 1990,
+		gender: 'female',
+	},
+	{
+		firstName: 'John',
+		lastName: 'Obi',
+		yearOfBirth: 1984,
+		gender: 'male',
+	},
+	{
+		firstName: 'Jane',
+		lastName: 'Obi',
+		yearOfBirth: 1994,
+		gender: 'female',
+	},
+	{
+		firstName: 'Margay',
+		lastName: 'Stonework',
+		yearOfBirth: 1996,
+		gender: 'female',
+	},
+	{
+		firstName: 'Mackle',
+		lastName: 'Johnston',
+		yearOfBirth: 2000,
+		gender: 'female',
+	},
+	{
+		firstName: 'Hessian',
+		lastName: 'Nwoke',
+		yearOfBirth: 1992,
+		gender: 'female',
+	},
+	{
+		firstName: 'Boris',
+		lastName: 'Johnson',
+		yearOfBirth: 1952,
+		gender: 'male',
+	},
+	{
+		firstName: 'Bordon',
+		lastName: 'Crackle',
+		yearOfBirth: 1975,
+		gender: 'male',
+	},
+	{
+		firstName: 'Futon',
+		lastName: 'Freckle',
+		yearOfBirth: 1880,
+		gender: 'female',
+	},
+];
+
